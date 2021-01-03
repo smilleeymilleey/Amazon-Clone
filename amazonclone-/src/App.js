8b18a970a18f453c7b0dd54449be9b3d636378c0
unknown <caitlynmiley.cm@gmail.com>
2021-01-03 04:40:35 -0600

    installed react router to be able to create multiple pages. created checkout component

--- amazonclone-/src/App.js
+++ amazonclone-/src/App.js
@@ -2,18 +2,30 @@ import React from "react";
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route }
+from "react-router-dom";
+import Checkout from './Checkout';
 
 function App() {
   return (
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          
+          </Route>
 
-    <div className="app">
-     
 
-      <Header />
-      <Home />
-   
-    
-    </div>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+
+    </Router>
   );
 }
 
