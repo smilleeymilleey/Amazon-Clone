db4d6a02f3b4d3ab559c0ce00a017bc56b3eb4dc
unknown <caitlynmiley.cm@gmail.com>
2021-01-02 21:25:59 -0600

    added the parts of a product to home page

--- amazonclone-/src/Home.js
+++ amazonclone-/src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./Home.css";
+import Product from "./Product";
 
 function Home() {
     return (
@@ -11,8 +12,8 @@ function Home() {
                  />
 
                  <div className="home__row">
-                    {/* Product*/}
-                    {/* Product*/}
+                    <Product />
+                    
                 </div>
                 <div className="home_row">
                     {/* Product*/}
@@ -25,9 +26,6 @@ function Home() {
 
 
                  </div>
-
-
-
             </div>
         </div>
     )
---
+++ amazonclone-/src/Product.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import "./Product.css";
+
+function Product() {
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>The Lean Startup</p>
+                <p className="product__price">
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className="product__rating">
+                    <p>*</p>
+                    <p>*</p>
+                    <p>*</p>  
+                </div>
+            </div>
+
+            <img
+                src="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+                alt="the lean startup book"
+            />
+
+            <button> Add to Basket </button>
+
+        </div>
+    )
+}
+
+export default Product
