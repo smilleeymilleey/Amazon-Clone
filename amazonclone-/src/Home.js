3e7090bc4f4f5529818f3156a6d2ad01da6f7789
unknown <caitlynmiley.cm@gmail.com>
2021-01-02 20:49:43 -0600

    styled header image to create a fade and set up rows for products

--- amazonclone-/src/Home.js
+++ amazonclone-/src/Home.js
@@ -5,7 +5,29 @@ function Home() {
     return (
         <div className="home">
             <div className="home__container">
-                <img src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Nzg3NzIxZDct/Nzg3NzIxZDct-ZjRkODFlZTkt-w1500._CB412095875_.jpg" alt="" />
+                <img 
+                className="home__image"
+                src="https://images-na.ssl-images-amazon.com/images/G/01/kindle/journeys/Nzg3NzIxZDct/Nzg3NzIxZDct-ZjRkODFlZTkt-w1500._CB412095875_.jpg" alt="amazonimage"
+                 />
+
+                 <div className="home__row">
+                    {/* Product*/}
+                    {/* Product*/}
+                </div>
+                <div className="home_row">
+                    {/* Product*/}
+                    {/* Product*/}
+                    {/* Product*/}
+                 </div>
+                 <div className="home__row">
+                    {/* Product*/}
+                    {/* Product*/}
+
+
+                 </div>
+
+
+
             </div>
         </div>
     )
