970e10046d1f20aa8d0a6901c8962345dd0f5c9a
unknown <caitlynmiley.cm@gmail.com>
2021-01-04 06:55:05 -0600

    styled the right checkout page

--- amazonclone-/src/Subtotal.js
+++ amazonclone-/src/Subtotal.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import "./Subtotal.css";
+import CurrencyFormat from "react-currency-format"; 
+import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 
 function Subtotal() {
     return (
-        <div>
-            
+        <div className="subtotal">
+            <CurrencyFormat
+            renderText={(value) => (
+                <>
+                 <p>
+                    {/*pass in the value prop*/}
+                  Subtotal (0 items): 
+                   <strong>0</strong>
+                 </p>
+                    <small className="subtotal__gift">
+                        <input type="checkbox" /> 
+                        This Order Contains A Gift
+                    </small>
+                </>
+            )}
+            decimalScale={2}
+            value={0} //* part of the homework*
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"$"}
+
+
+            />
+
+         <button>Proceed To Checkout</button>
         </div>
     )
 }
