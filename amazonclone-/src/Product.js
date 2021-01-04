c6b58d39a961df7d28bcb6a421db2be1d25e2ebd
unknown <caitlynmiley.cm@gmail.com>
2021-01-04 08:31:00 -0600

    refined all files, and successfully installed api to add products to the basket

--- amazonclone-/src/Product.js
+++ amazonclone-/src/Product.js
@@ -1,36 +1,60 @@
-import React from 'react';
-import "./Product.css";
+
 import StarRateIcon from '@material-ui/icons/StarRate';
+import React from "react";
+import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
-function Product({ title, image, price, rating }) {
-    return (
-        <div className="product">
-            <div className="product__info">
-                <p>{title}</p>
-                <p className="product__price">
-                    <small>$</small>
-                    <strong>{price}</strong>
-                </p>
-                <div className="product__rating">
-                    {Array (rating).fill().map((_, i) => (
+function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
 
-                    <StarRateIcon />
-                    ))}
+  
 
-                   
-                   
-                </div>
-            </div>
+  const addToBasket = () => {
+    // dispatch the item into the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
 
-            <img
-                src={image}
-                alt="amazon product picture"
-            />
-
-            <button> Add to Basket </button>
-
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+            <StarRateIcon />
+            ))}
         </div>
-    )
+      </div>
+
+      <img src={image} alt="" />
+
+      <button onClick={addToBasket}>Add to Basket</button>
+    </div>
+  );
 }
 
-export default Product
+export default Product;
+    
+
+    
+    
+    
+    
+
+
+                
+                    
