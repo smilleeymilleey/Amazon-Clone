60d173d8c3d028bd3aeabe7a5a6200ab5511f24d
unknown <caitlynmiley.cm@gmail.com>
2021-01-05 12:04:01 -0600

    styled products in basket

--- amazonclone-/src/Checkout.js
+++ amazonclone-/src/Checkout.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Checkout.css';
 import Subtotal from "./Subtotal";
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct';
 
 
 
