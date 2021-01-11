b4f92aae08fe9af623dddbf890c171df1e6e4acb
Caitlyn <caitlynmiley.cm@gmail.com>
2021-01-11 09:16:21 -0600

    used firebase to create a user authentication/login page

--- amazonclone-/src/firebase.js
+++ amazonclone-/src/firebase.js
@@ -1,4 +1,7 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyA7x0Hi60lGnvOT1TYZV_gwECW8UDdXTxQ",
     authDomain: "e-commerce-34a07.firebaseapp.com",
@@ -8,3 +11,11 @@ const firebaseConfig = {
     appId: "1:49438603242:web:2334e2708117023181e493",
     measurementId: "G-GSKSTP7KWW"
   };
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  
+  export { db, auth };
